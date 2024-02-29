78848e776acb0bb1284ace5385e3a1e6031255b1
Matthias Denu <matthiasdenu@outlook.com>
2024-02-29 04:37:35 -0500

    Coin Change (Dirty)

--- coin_change_dirty.py
+++ coin_change_dirty.py
@@ -30,70 +30,70 @@ class CoinPath:
         return "".join([repr(x) for x in sorted(self._path)])
 
     def __str__(self):
-        return self.__repr__()
+        return "|".join([str(v) for v in self._path])
 
 
-def coin_change(coins, amount):
-    if amount == 0:
-        return 0
+class Solution:
 
-    small_count = float('inf')
-    small_coin = min(coins)
-    unvisited = [CoinPath([denom]) for denom in coins]
-    candidates = []
-    visited = []
+    @staticmethod
+    def coinChange(coins, amount) -> int:
 
-    while unvisited:
-        curr_path = unvisited.pop()
-        leftover = amount - curr_path.amount
-        if curr_path.count < small_count:
+        coins_sorted = list(sorted(coins))
+        small_coin = coins_sorted[0]
+
+        if amount < small_coin:
+            return 0
+
+        unvisited = [CoinPath([denom]) for denom in coins_sorted]  # Reverse! Reverse!
+        while unvisited:
+            curr_path = unvisited.pop()
+            remainder = amount - curr_path.amount
             if curr_path.amount == amount:  # Found a path to amount
-                candidates.append(curr_path)
-                small_count = curr_path.count
-                continue
+                # from collections import Counter
+                # print(f"Solution found: {Counter(str(curr_path).split('|'))}")
+                return curr_path.count
             elif curr_path.amount > amount:  # Amount exceeded
                 continue
-            elif leftover < small_coin:  # This path leads nowhere
+            elif remainder < small_coin:  # This path leads nowhere
                 continue
             elif curr_path.amount < amount:  # Keep looking
-                for denom in [c for c in coins if c <= leftover]:
+                for denom in [c for c in coins_sorted if c <= remainder]:
                     new_path = CoinPath(curr_path.path + [denom])  # TODO Refactor so not instantiating every time
-                    if not (new_path in visited):
-                        visited.append(new_path)
+                    if not (new_path in unvisited):
                         unvisited.append(new_path)
 
-    result = -1
-    if candidates:
-        result = min([c.count for c in candidates])
-
-    return result
+        return -1
 
 
 if __name__ == "__main__":
+    sol = Solution()
+
     coins = [1, 2, 5]
     amount = 11
     expected = 3
-    actual = coin_change(coins, amount)
+    actual = sol.coinChange(coins, amount)
     assert actual == expected
 
     coins = [2]
     amount = 3
     expected = -1
-    actual = coin_change(coins, amount)
+    actual = sol.coinChange(coins, amount)
     assert actual == expected
 
     coins = [1]
     amount = 0
     expected = 0
-    actual = coin_change(coins, amount)
+    actual = sol.coinChange(coins, amount)
     assert actual == expected
 
     coins = [1, 2, 5]
     amount = 100
     expected = 20
-    actual = coin_change(coins, amount)
+    actual = sol.coinChange(coins, amount)
     assert actual == expected
 
-    coins = [186,419,83,408]
-    amount = 6249
-    print(coin_change(coins, amount))
+    # coins = [186, 419, 83, 408]
+    # amount = 6249
+    # expected = 20
+    # actual = sol.coinChange(coins, amount)
+    # assert actual == expected

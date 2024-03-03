34a95f2dd1a69cdba3b6012ff9c4cff54e1baf8e
Matthias Denu <matthiasdenu@outlook.com>
2024-03-03 11:49:41 -0500

    Merged Sorted Array

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
     m = 3
     nums2 = [2, 5, 6]
     n = 3
-
     sol.merge(nums1, m, nums2, n)
+    print(nums1)
---
+++ merge_sorted_array_cheat.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/merge-sorted-array
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:] = nums2[:]
+        nums1.sort()
+
+
+if __name__ == '__main__':
+    sol = Solution()
+
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+    sol.merge(nums1, m, nums2, n)
+    print(nums1)

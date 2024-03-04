c3eb65f4d3b118e066ee600845ca5b3c8a79634f
Matthias Denu <matthiasdenu@outlook.com>
2024-03-04 17:55:18 -0500

    3Sum (Time limit exceeded)

--- three_sum.py
+++ three_sum.py
@@ -7,26 +7,18 @@ from collections import defaultdict
 class Solution:
     def threeSum(self, nums: list[int]) -> list[tuple[int | list[int], ...]]:
         # Build pairs dict
+        nums = sorted(nums)
         pairs = defaultdict(list[int])
         for ii in range(len(nums)):
             for jj in range(ii + 1, len(nums)):
                 pairs[nums[ii] + nums[jj]].append((ii, jj))
 
         # Find answer
-        result = []
-        seen_idx = []
-        seen_val = []
+        result = set()
         for kk, num in enumerate(nums):
             for cand in pairs[-num]:
                 if kk not in cand:
-                    # Just get it done and move on.
-                    # Look up the optimal solution afterward.
-                    temp_idx = tuple(sorted(cand + (kk,)))
-                    temp_val = tuple(sorted(nums[idx] for idx in temp_idx))
-                    if temp_idx not in seen_idx and temp_val not in seen_val:
-                        seen_idx.append(temp_idx)
-                        seen_val.append(temp_val)
-                        result.append(temp_val)
+                    result.add(tuple(nums[idx] for idx in tuple(sorted(cand + (kk,)))))
 
         return result
 

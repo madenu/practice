0e908b4913abf5b19a4a4fba7ac2118c1a76bc55
Matthias Denu <matthiasdenu@outlook.com>
2024-03-08 08:05:44 -0500

    Rotate Array

--- rotate_array.py
+++ rotate_array.py
@@ -1,26 +1,24 @@
-# # Rotate Array
-# # https://leetcode.com/problems/rotate-array/
-#
-# # WORK IN PROGRESS
-# class Solution:
-#     def rotate(self, nums: list[int], k: int) -> None:
-#         """
-#         Do not return anything, modify nums in-place instead.
-#         """
-#         len_nums = len(nums)
-#         while k > 0:
-#             next_k = k % len_nums
-#             k = k // nums
-#             end = nums[-k:]
-#             nums[k:] = nums[:len_nums-k]
-#             nums[:k] = end
-#             k = next_k
-#
-#
-# if __name__ == '__main__':
-#     sol = Solution()
-#     nums = [1, 2, 3, 4, 5, 6, 7]
-#     k = 3
-#     expected = [5, 6, 7, 1, 2, 3, 4]
-#     sol.rotate(nums, k)
-#     print(nums)
+# Rotate Array
+# https://leetcode.com/problems/rotate-array/
+
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        len_nums = len(nums)
+        k = k % len_nums
+        if k == 0 or len_nums < 2:
+            return
+        end = nums[-k:]
+        nums[k:] = nums[:len_nums - k]
+        nums[:k] = end
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    k = 3
+    expected = [5, 6, 7, 1, 2, 3, 4]
+    sol.rotate(nums, k)
+    print(nums)
